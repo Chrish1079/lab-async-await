8317fce1c2d59541cd5b037612ae03059ed8a35c
Chrish1079 <itssuper1079@gmail.com>
2025-10-24 16:24:48 -0400

    submit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your code here!
+function displayPosts(posts) {
+    const postList = document.getElementById('post-list');
+    
+    posts.forEach(post => {
+        
+        const li = document.createElement('li');
+        
+        const h1 = document.createElement('h1');
+        h1.textContent = post.title;
+        
+        const p = document.createElement('p');
+        p.textContent = post.body;
+        
+        li.appendChild(h1);
+        li.appendChild(p);
+        
+        postList.appendChild(li);
+    });
+}
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(posts => {
+        displayPosts(posts);
+    });
